85f7da9beaacde5ecbd8c493e760a541b14eadd6
Braulio <braulio.esmu@gmail.com>
2020-09-01 20:49:10 -0400

    Recovery DB

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -25,7 +25,7 @@ class SchoolsController < ApplicationController
   # POST /schools
   # POST /schools.json
   def create
-    @school = School.new(school_params.merge(user: current_user))
+    @school = School.new(school_params.merge(user_id: current_user))
 
     respond_to do |format|
       if @school.save
